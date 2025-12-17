dc80276e70952f0ced6e1bbf54135491111839a1
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-17 13:22:48 -0300

    feat: Create the category enum

---
+++ src/main/java/com/github/arthurscarpin/MagicFridgeAI/enums/FoodItemCategory.java
@@ -0,0 +1,14 @@
+package com.github.arthurscarpin.MagicFridgeAI.enums;
+
+public enum FoodItemCategory {
+    VEGETABLES,
+    FRUITS,
+    PROTEINS,
+    DAIRY,
+    PROCESSED_FOODS,
+    BREADS_AND_PASTA,
+    CONDIMENTS,
+    DESSERTS,
+    BEVERAGES,
+    FROZEN_FOODS
+}
